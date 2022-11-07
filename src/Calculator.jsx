e3e74470ab14a2e2c80767a4c4aa55eb744c4ca5
Deven <r.deven.kane@gmail.com>
2022-11-06 19:52:33 -0800

    add Display to Calculator + styling Display
    running total state was added too!

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,7 +1,13 @@
+import {useState} from 'react';
 import './Calculator.css';
+import Display from './Display';
+
 function Calculator() {
+  const [total, setTotal] = useState(0);
+
   return (
     <div className="Calculator">
+      <Display total={total} />
     </div>
   );
 }
---
+++ src/Display.jsx
@@ -0,0 +1,11 @@
+import './Display.css';
+
+function Display({total}) {
+  return (
+    <div className="Display">
+      <span> {total} </span>
+    </div>
+  );
+}
+
+export default Display;
