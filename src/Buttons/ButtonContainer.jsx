82332c5655eef0075821c155ff70584f099b6324
Deven <r.deven.kane@gmail.com>
2022-11-06 20:15:31 -0800

    add Operand to ButtonContainer + styling

--- src/Buttons/ButtonContainer.jsx
+++ src/Buttons/ButtonContainer.jsx
@@ -1,9 +1,10 @@
 import './ButtonContainer.css';
+import Operand from './Operand/Operand.jsx';
 
 function ButtonContainer({total, setTotal}) {
-
   return (
     <div className="ButtonContainer">
+      <Operand total={total} setTotal={setTotal} />
     </div>
   );
 }
---
+++ src/Buttons/Operand/Operand.jsx
@@ -0,0 +1,7 @@
+import './Operand.css';
+
+function Operand({total, setTotal}) {
+  return <button className="SquareButton">1</button>;
+}
+
+export default Operand;
