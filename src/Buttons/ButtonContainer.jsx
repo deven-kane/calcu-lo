a96f8dd01398bb3d5696c4c440243ba4555ed769
Deven <r.deven.kane@gmail.com>
2022-11-11 18:42:24 -0800

    render all Operands

--- src/Buttons/ButtonContainer.jsx
+++ src/Buttons/ButtonContainer.jsx
@@ -1,10 +1,13 @@
 import './ButtonContainer.css';
 import Operand from './Operand/Operand.jsx';
 
+const OPERANDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 function ButtonContainer({total, setTotal}) {
   return (
     <div className="ButtonContainer">
-      <Operand total={total} setTotal={setTotal} />
+      {OPERANDS.map((operand) => {
+        return <Operand total={total} setTotal={setTotal} operand={operand} />;
+      })}
     </div>
   );
 }
--- src/Buttons/Operand/Operand.jsx
+++ src/Buttons/Operand/Operand.jsx
@@ -1,13 +1,13 @@
 import './Operand.css';
 
-function Operand({total, setTotal}) {
+function Operand({total, setTotal, operand}) {
   function handleClick() {
     setTotal(total + 1);
   }
 
   return (
     <button onClick={handleClick} className="SquareButton">
-      1
+      {operand}
     </button>
   );
 }
